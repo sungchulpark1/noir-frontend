4c69dc27879046c91b52c754fa2bb23d99268313
Stephen Park <spark0843@gmail.com>
2025-07-03 14:08:39 -0400

    rm home tab visiblity

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -42,6 +42,7 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          href: null,
         }}
       />
     </Tabs>
